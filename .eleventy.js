153fea4242489f1f7cce0f9ce03c310db870c7bc
Mike <helmers.mike@gmail.com>
2024-07-12 15:15:44 -0500

    Initial

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("images");
+  
+    return {
+      passthroughFileCopy: true,
+      dir: {
+        input: ".",
+        includes: "_includes",
+        data: "_data",
+        output: "_site"
+      }
+    };
+  };
+  
