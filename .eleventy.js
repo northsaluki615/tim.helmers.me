05b087347f2972e37803ff26271c56e808931ca9
Michael Helmers <mikehelmers@proton.me>
2024-07-24 10:01:44 -0500

    feat: Add dark mode functionality to the website

--- .eleventy.js
+++ .eleventy.js
@@ -8,7 +8,7 @@ module.exports = function(eleventyConfig) {
         input: ".",
         includes: "_includes",
         data: "_data",
-        output: "_site"
+        output: "_static"
       }
     };
   };
